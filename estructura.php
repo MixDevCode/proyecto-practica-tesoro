8bc020ae53cdb1dbc2b5197f6a0704536b4c3e7c
Dante <sopaelbiologo@gmail.com>
2022-06-15 20:48:44 -0300

    preguntas agregadas

--- estructura.php
+++ estructura.php
@@ -1,5 +1,4 @@
 <?php
-
 $stands = array(
     "SF1" => "Software",
     "INF07" => "Infraestructura",
@@ -8,13 +7,25 @@ $stands = array(
 
 $preguntas = array(
     "SF1" => array(
-        "Formato de imagen", "Formato de página web", "¿Qué significa la sigla S.O.?", "¿Que es internet?", "¿Que es un sotware libre?"
-        ),
+        "Formato de imagen",
+        "Formato de página web",
+        "¿Qué significa la sigla S.O.?",
+        "¿Que es internet?",
+        "¿Que es YouTube?"
+    ) ,
     "INF07" => array(
-        "Pregunta1", "Pregunta2", "Pregunta3", "Pregnta4", "Pregunta5"
-    ),
+        "Pregunta1",
+        "Pregunta2",
+        "Pregunta3",
+        "Pregnta4",
+        "Pregunta5"
+    ) ,
     "BIO72" => array(
-        "Pregunta1", "Pregunta2", "Pregunta3", "Pregnta4", "Pregunta5"
+        "Pregunta1",
+        "Pregunta2",
+        "Pregunta3",
+        "Pregnta4",
+        "Pregunta5"
     )
 );
 
@@ -23,55 +34,133 @@ $respuestas = array(
         "Formato de imagen" => array(
             "correcta" => "jpg",
             "posibles" => array(
-            "http", "php", "jgp", "peg", "jpg"
-            ),
-        ),
+                "http",
+                "php",
+                "jgp",
+                "peg",
+                "jpg"
+            ) ,
+        ) ,
         "Formato de página web" => array(
-            "jpg", "MIDI", "http", "python", "html"
-        ),
+            "correcta" => "html",
+            "posibles" => array(
+                "jpg",
+                "MIDI",
+                "http",
+                "python",
+                "html"
+            )
+        ) ,
         "¿Qué significa la sigla S.O.?" => array(
-            "Sincronización Óptima", "Sistema Operativo", "Standard Operation", "Sostén de Objeto", "Sintaxis.org"
-        ),
+            "correcta" => "Sistema Operativo",
+            "posibles" => array(
+                "Sincronización Óptima",
+                "Sistema Operativo",
+                "Standard Operation",
+                "Sostén de Objeto",
+                "Sintaxis.org"
+            ) ,
+        ) ,
         "¿Que es internet?" => array(
-            "Una red global", "Es la Web", "Un protocolo de conexión", "Una red local", "Es un programa"
-        ),
-        "pregunta5" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
-    ),
-    "Infraestructura" => array(
-        "Pregunta1" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
+            "correcta" => "Una red mundial",
+            "posibles" => array(
+                "Una red mundial",
+                "Es la Web",
+                "WiFi",
+                "Una red hogar",
+                "Es un programa"
+            ) ,
+        ) ,
+        "¿Qué es YouTube?" => array(
+            "correcta" => "Una página web de videos",
+            "posibles" => array(
+                "Una página web de videos",
+                "Una red social",
+                "Un videojuego",
+                "Una canción",
+                "Una foto"
+            ) ,
+        ) ,
+    ) ,
+    "INF07" => array(
+        "¿Qué es el Hardware?" => array(
+            "correcta" => "Todo lo que se toca de una computadora",
+            "posibles" => array(
+                "Un programa",
+                "Todo lo que se toca de una computadora",
+                "Un teclado",
+                "La persona que trabaja con la PC",
+                "Un procesador"
+            )
+        ) ,
+        "¿Qué es lo más esencial de una computadora?" => array(
+            "correcta" => "RAM",
+            "posibles" => array(
+                "Disco duro",
+                "Google",
+                "Memoria Caché",
+                "Bios",
+                "RAM"
+            ) ,
+        ) ,
+        "¿Qué es un CPU?" => array(
+            "Una computadora",
+            "Gabinete",
+            "Procesador",
+            "Una ONG",
+            "Una herramienta"
+        ) ,
+        "¿Qué es un monitor?" => array(
+            "Una pantalla",
+            "Una televiión",
+            "Una computadora",
+            "Un programa",
+            "Un parlante"
+        ) ,
+        "¿Quién arregla una computadora?" => array(
+            "Un electriciista",
+            "Un técnico",
+            "Un YouTuber",
+            "Un programador",
+            "Una maestra"
+        ) ,
+    ) ,
+    "Turismo" => array(
+        "¿Cuántas comparsas hay?" => array(
+            "1",
+            "3",
+            "5",
+            "7",
+            "10"
+        ) ,
         "Pregunta2" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
+            "Respuesta1",
+            "Respuesta2",
+            "Respuesta3",
+            "Respuesta4",
+            "Respuesta5"
+        ) ,
         "Pregunta3" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
+            "Respuesta1",
+            "Respuesta2",
+            "Respuesta3",
+            "Respuesta4",
+            "Respuesta5"
+        ) ,
         "pregunta4" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
+            "Respuesta1",
+            "Respuesta2",
+            "Respuesta3",
+            "Respuesta4",
+            "Respuesta5"
+        ) ,
         "pregunta5" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
-    ),
-    "Biologia" => array(
-        "Pregunta1" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
-        "Pregunta2" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
-        "Pregunta3" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
-        "pregunta4" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
-        "pregunta5" => array(
-            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
-        ),
+            "Respuesta1",
+            "Respuesta2",
+            "Respuesta3",
+            "Respuesta4",
+            "Respuesta5"
+        ) ,
     )
 );
 ?>
