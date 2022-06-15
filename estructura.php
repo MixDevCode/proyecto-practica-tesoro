d2795993ab504234b89e72d93df0b532f17c28ac
Mix <txtbmx_teen@live.com>
2022-06-15 16:45:14 -0300

    maquetado de la página

---
+++ estructura.php
@@ -0,0 +1,66 @@
+<?php
+$preguntas = array(
+    "Software" => array(
+        "Formato de imagen", "Formato de página web", "¿Qué significa la sigla S.O.?", "¿Que es internet?", "¿Que es un sotware libre?"
+        ),
+    "Infraestructura" => array(
+        "Pregunta1", "Pregunta2", "Pregunta3", "Pregnta4", "Pregunta5"
+    ),
+    "Biologia" => array(
+        "Pregunta1", "Pregunta2", "Pregunta3", "Pregnta4", "Pregunta5"
+    )
+);
+
+$respuestas = array(
+    "Software" => array(
+        "Formato de imagen" => array(
+            "http", "php", "jgp", "peg", "jpg"
+        ),
+        "Formato de página web" => array(
+            "jpg", "MIDI", "http", "python", "html"
+        ),
+        "¿Qué significa la sigla S.O.?" => array(
+            "Sincronización Óptima", "Sistema Operativo", "Standard Operation", "Sostén de Objeto", "Sintaxis.org"
+        ),
+        "¿Que es internet?" => array(
+            "Una red global", "Es la Web", "Un protocolo de conexión", "Una red local", "Es un programa"
+        ),
+        "pregunta5" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+    ),
+    "Infraestructura" => array(
+        "Pregunta1" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+        "Pregunta2" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+        "Pregunta3" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+        "pregunta4" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+        "pregunta5" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+    ),
+    "Biologia" => array(
+        "Pregunta1" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+        "Pregunta2" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+        "Pregunta3" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+        "pregunta4" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+        "pregunta5" => array(
+            "Respuesta1", "Respuesta2", "Respuesta3", "Respuesta4", "Respuesta5"
+        ),
+    )
+);
